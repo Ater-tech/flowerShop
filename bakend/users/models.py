4c16ebf18fe4a2839c2eff90d6f1aaa8aff5b885
Ater-tech <jabborovyulchiboy@gmail.com>
2026-09-16 21:51:14 +0500

    page detail finished 2

--- bakend/users/models.py
+++ bakend/users/models.py
@@ -2,4 +2,4 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
-    pass
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
